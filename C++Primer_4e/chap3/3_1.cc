067ee1afa7ec8116e14d60942331670a6a739119
NicoleRobin <lit050528@gmail.com>
2015-10-25 19:35:42 +0800

    add chap3

---
+++ C++Primer_4e/chap3/3_1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string str = "test";
+    cout << str.length() << endl;
+    string strTest("test");
+    cout << strTest.length() << endl;
+
+    return 0;
+}
